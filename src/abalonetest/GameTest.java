99252ce74ae7c45f13d80cf3e48fb6b168ad5c55
Dylan Janssen <d.d.janssen@student.utwente.nl>
2023-03-17 15:33:52 +0100

    add game test

---
+++ src/abalonetest/GameTest.java
@@ -0,0 +1,22 @@
+package abalonetest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class GameTest {
+
+    /**
+     * Set up before each test case.
+     */
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void isField() {
+
+    }
+
+}
