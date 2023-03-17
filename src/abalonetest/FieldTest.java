fd3326c1b883e5d58c44051a87817195a1f70d4e
Dylan Janssen <d.d.janssen@student.utwente.nl>
2023-03-17 15:03:58 +0100

    setup for junits tests

---
+++ src/abalonetest/FieldTest.java
@@ -0,0 +1,31 @@
+package abalonetest;
+
+import abalonecontrol.LocalGame;
+import abalonemodel.Board;
+import abalonemodel.Field;
+import abalonemodel.Mark;
+import abalonemodel.Move;
+import abaloneview.Tui;
+import abaloneview.View;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class FieldTest {
+
+    /**
+     * Set up before each test case.
+     */
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void isField() {
+
+    }
+
+}
