a3a1c71756d8eff2089c97cb9b09186a6a36c43a
sophieweid <s.weidmann@student.utwente.nl>
2023-03-21 16:23:45 +0100

    FieldTest.java and MarkTest.java with 100% coverage

--- src/abalonetest/FieldTest.java
+++ src/abalonetest/FieldTest.java
@@ -4,7 +4,6 @@ import abalonecontrol.LocalGame;
 import abalonemodel.Board;
 import abalonemodel.Field;
 import abalonemodel.Mark;
-import abalonemodel.Move;
 import abaloneview.Tui;
 import abaloneview.View;
 import org.junit.Before;
@@ -15,17 +14,81 @@ import static org.junit.Assert.*;
 
 public class FieldTest {
 
+    String[] twoPlayers = {"dylan", "anissa"};
+    Board board;
+    View view = new Tui();
+
     /**
      * Set up before each test case.
      */
     @Before
     public void setUp() {
-
+        board = (new LocalGame(twoPlayers, view)).getBoard();
     }
 
+
+    /* General tests */
+
+    /*
+    * Test that the field is a Field.
+    * */
     @Test
     public void isField() {
-
+        assertTrue(board.isField(11));
+        assertTrue(board.isField(99));
+        assertFalse(board.isField(1));
+        for (Field f : board.getFields()) {
+            assertTrue(board.isField(f.getIndex()));
+        }
     }
 
+    /*
+    * Test the getMark() method. It either returns Mark.X or Mark.B or Mark.C or Mark.W or Mark.R.
+    * */
+    @Test
+    public void getMark() {
+        for (Field f : board.getFields()) {
+            assertTrue(f.getMark() == Mark.X || f.getMark() == Mark.B || f.getMark() == Mark.C || f.getMark() == Mark.W || f.getMark() == Mark.R);
+        }
+    }
+
+    /*
+    * Test that the setMark() method actually sets the mark.
+    * Test with Mark.X, Mark.B, Mark.C, Mark.W and Mark.R.
+    * */
+    @Test
+    public void setMark() {
+        for (Field f : board.getFields()) {
+            f.setMark(Mark.X);
+            assertEquals(f.getMark(), Mark.X);
+            f.setMark(Mark.B);
+            assertEquals(f.getMark(), Mark.B);
+            f.setMark(Mark.C);
+            assertEquals(f.getMark(), Mark.C);
+            f.setMark(Mark.W);
+            assertEquals(f.getMark(), Mark.W);
+            f.setMark(Mark.R);
+            assertEquals(f.getMark(), Mark.R);
+        }
+    }
+
+    /*
+    * Test the getIndex() method.
+    * */
+    @Test
+    public void getIndex() {
+        for (Field f : board.getFields()) {
+            assertEquals(f.getIndex(), f.getIndex());
+        }
+    }
+
+    /*
+     * Test that the isEmptyField() is actually marked as empty (Mark.X).
+     * */
+    @Test
+    public void isEmptyField() {
+        for (Field f : board.getFields()) {
+            assertEquals(f.isEmptyField(), f.getMark() == Mark.X);
+        }
+    }
 }
--- src/abalonetest/MarkTest.java
+++ src/abalonetest/MarkTest.java
@@ -1,10 +1,7 @@
 package abalonetest;
 
 import abalonecontrol.LocalGame;
-import abalonemodel.Board;
-import abalonemodel.Field;
-import abalonemodel.Mark;
-import abalonemodel.Move;
+import abalonemodel.*;
 import abaloneview.Tui;
 import abaloneview.View;
 import org.junit.Before;
@@ -15,17 +12,108 @@ import static org.junit.Assert.*;
 
 public class MarkTest {
 
+
+    String[] twoPlayers = {"a", "b"};
+    String[] threePlayers = {"a", "b", "c"};
+    String[] fourPlayers = {"a", "b", "c", "d"};
+    View view = new Tui();
+    Board twoboard;
+    Board threeboard;
+    Board fourboard;
+
+
     /**
      * Set up before each test case.
      */
     @Before
     public void setUp() {
-
+        twoboard = (new LocalGame(twoPlayers, view)).getBoard();
+        threeboard = (new LocalGame(threePlayers, view)).getBoard();
+        fourboard = (new LocalGame(fourPlayers, view)).getBoard();
     }
 
+    /* General tests */
+
+    /*
+    * Test that the class Mark is an enum class.
+    * */
     @Test
-    public void isField() {
-
+    public void isEnum() {
+        assertTrue(Mark.class.isEnum());
     }
 
+    /*
+    * Test the enums of class Mark.
+    * */
+    @Test
+    public void testEnums() {
+        assertEquals(Mark.X, Mark.valueOf("X"));
+        assertEquals(Mark.B, Mark.valueOf("B"));
+        assertEquals(Mark.C, Mark.valueOf("C"));
+        assertEquals(Mark.W, Mark.valueOf("W"));
+        assertEquals(Mark.R, Mark.valueOf("R"));
+    }
+
+    /*
+    * Test that Mark getNextPlayerTwo() returns the other mark when
+    * there are two players. Test with Mark.B and Mark.W.
+    * If the Mark is XX, the method should return XX.
+    * */
+    @Test
+    public void getNextPlayerTwo() {
+        final String[] twoPlayers = {"Computer", "Computer"};
+        LocalGame game = new LocalGame(twoPlayers, new Tui());
+        assertEquals(Mark.W, game.getCurrentPlayer().getMark().getNextPlayerTwo());
+        ComputerPlayer computerPlayer = (ComputerPlayer) game.getCurrentPlayer();
+        Move move = computerPlayer.makeMove(twoboard);
+        assertTrue(twoboard.validMove(move));
+        twoboard.executeMove(move);
+        game.currentPlayer = game.getNextPlayer();
+        assertEquals(Mark.B, game.getCurrentPlayer().getMark().getNextPlayerTwo());
+        assertEquals(Mark.X, Mark.X.getNextPlayerTwo());
+    }
+
+    /*
+    * Test that Mark getNextPlayerThree() returns the other mark when
+    * there are three players. Test with Mark.B, Mark.C and Mark.W.
+    * If the Mark is XX, the method should return XX.
+    * */
+    @Test
+    public void getNextPlayerThree() {
+        final String[] threePlayers = {"Computer", "Computer", "Computer"};
+        LocalGame game = new LocalGame(threePlayers, new Tui());
+        assertEquals(Mark.C, game.getCurrentPlayer().getMark().getNextPlayerThree());
+        ComputerPlayer computerPlayer = (ComputerPlayer) game.getCurrentPlayer();
+        Move move = computerPlayer.makeMove(threeboard);
+        assertTrue(threeboard.validMove(move));
+        threeboard.executeMove(move);
+        game.currentPlayer = game.getNextPlayer();
+        assertEquals(Mark.W, game.getCurrentPlayer().getMark().getNextPlayerThree());
+        game.currentPlayer = game.getNextPlayer();
+        assertEquals(Mark.B, game.getCurrentPlayer().getMark().getNextPlayerThree());
+        assertEquals(Mark.X, Mark.X.getNextPlayerThree());
+    }
+
+    /*
+    * Test that Mark getNextPlayerFour() returns the other mark when
+    * there are four players. Test with Mark.B, Mark.C, Mark.W and Mark.R.
+    * If the Mark is XX, the method should return XX.
+    * */
+    @Test
+    public void getNextPlayerFour() {
+        final String[] fourPlayers = {"Computer", "Computer", "Computer", "Computer"};
+        LocalGame game = new LocalGame(fourPlayers, new Tui());
+        assertEquals(Mark.C, game.getCurrentPlayer().getMark().getNextPlayerFour());
+        ComputerPlayer computerPlayer = (ComputerPlayer) game.getCurrentPlayer();
+        Move move = computerPlayer.makeMove(fourboard);
+        assertTrue(fourboard.validMove(move));
+        fourboard.executeMove(move);
+        game.currentPlayer = game.getNextPlayer();
+        assertEquals(Mark.W, game.getCurrentPlayer().getMark().getNextPlayerFour());
+        game.currentPlayer = game.getNextPlayer();
+        assertEquals(Mark.R, game.getCurrentPlayer().getMark().getNextPlayerFour());
+        game.currentPlayer = game.getNextPlayer();
+        assertEquals(Mark.B, game.getCurrentPlayer().getMark().getNextPlayerFour());
+        assertEquals(Mark.X, Mark.X.getNextPlayerFour());
+    }
 }
