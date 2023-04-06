68d2c65afaaa98ae8bf852d5e56424af985c7d08
Dylan Janssen <d.d.janssen@student.utwente.nl>
2023-04-06 11:25:52 +0200

    Add: MoveTest

--- src/abalonetest/MoveTest.java
+++ src/abalonetest/MoveTest.java
@@ -10,22 +10,27 @@ import abaloneview.View;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 
 public class MoveTest {
 
-    /**
-     * Set up before each test case.
-     */
-    @Before
-    public void setUp() {
-
-    }
-
     @Test
     public void isField() {
-
+        for( int i = 0; i < 20; i++ ) {
+            //random int betwwn 0 and 6
+            int random = (int)(Math.random() * 7);
+            int randomRange = (int)(Math.random() * 3);
+            int[] marbles = new int[randomRange];
+            for( int j = 0; j < randomRange; j++ ) {
+                marbles[j] = (int)(Math.random() * 94);
+            }
+            Move move = new Move( marbles, random );
+            assertEquals( random, move.getDirection() );
+            assertEquals( marbles, move.getMarblePositions());
+        }
     }
 
 }
